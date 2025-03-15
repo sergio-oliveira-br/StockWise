87b96c68a227a1aa1db451ff546071c922e95d61
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-03-15 14:20:24 +0000

    Update ProductService.java
    act:
    -> new method created to find product by id

--- src/main/java/com/github/sergiooliveirabr/stockwise/service/ProductService.java
+++ src/main/java/com/github/sergiooliveirabr/stockwise/service/ProductService.java
@@ -5,6 +5,8 @@ import com.github.sergiooliveirabr.stockwise.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class ProductService {
@@ -22,5 +24,9 @@ public class ProductService {
 
     public Iterable<Products> findAll() {
         return productRepository.findAll();
+    }
+
+    public Optional<Products> findById(Long id) {
+        return productRepository.findById(id);
     }
 }
