c46d606e19484e0912beba3365e12d51b31721a7
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-03-15 14:23:30 +0000

    Update Sales.java
    Issue:
    # The Lombok approach is not working here
    
    Act:
    -> Lombok disabled
    -> setters and getters  inclued

--- src/main/java/com/github/sergiooliveirabr/stockwise/model/Sales.java
+++ src/main/java/com/github/sergiooliveirabr/stockwise/model/Sales.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import java.time.LocalDate;
 
 @Entity
-@Data
+//@Data
 @Table(name = "sales")
 public class Sales {
 
@@ -21,5 +21,46 @@ public class Sales {
     private int qty;
 
     private LocalDate salesDate;
-    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Products getProducts() {
+        return products;
+    }
+
+    public void setProducts(Products products) {
+        this.products = products;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public LocalDate getSalesDate() {
+        return salesDate;
+    }
+
+    public void setSalesDate(LocalDate salesDate) {
+        this.salesDate = salesDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Sales{" +
+                "id=" + id +
+                ", products=" + products +
+                ", qty=" + qty +
+                ", salesDate=" + salesDate +
+                '}';
+    }
 }
