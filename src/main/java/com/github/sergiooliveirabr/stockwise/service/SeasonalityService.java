c4e319528146265fab72b7291d6cde91aaebd4fa
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-03-17 21:59:39 +0000

    Create SeasonalityService.java
    act:
    -> basics method included (save, find all and get by id)

---
+++ src/main/java/com/github/sergiooliveirabr/stockwise/service/SeasonalityService.java
@@ -0,0 +1,31 @@
+package com.github.sergiooliveirabr.stockwise.service;
+
+import com.github.sergiooliveirabr.stockwise.model.Seasonality;
+import com.github.sergiooliveirabr.stockwise.repository.SeasonalityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SeasonalityService {
+
+    private final SeasonalityRepository seasonalityRepository;
+
+    @Autowired
+    public SeasonalityService(SeasonalityRepository seasonalityRepository) {
+        this.seasonalityRepository = seasonalityRepository;
+    }
+
+    public void saveSeasonality(Seasonality seasonality) {
+        seasonalityRepository.save(seasonality);
+    }
+
+    public Iterable<Seasonality> findAllSeasonality() {
+        return seasonalityRepository.findAll();
+    }
+
+    public Optional<Seasonality> getSeasonalityById(Long id) {
+        return seasonalityRepository.findById(id);
+    }
+}
