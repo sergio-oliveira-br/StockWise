29b2146c9f48f045cb705cf6224d95b6f1d68356
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-03-17 22:03:36 +0000

    Update IndexController.java
    act:
    -> included new attribute to update the index page

--- src/main/java/com/github/sergiooliveirabr/stockwise/controller/IndexController.java
+++ src/main/java/com/github/sergiooliveirabr/stockwise/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.github.sergiooliveirabr.stockwise.controller;
 
 import com.github.sergiooliveirabr.stockwise.service.ProductService;
 import com.github.sergiooliveirabr.stockwise.service.SaleService;
+import com.github.sergiooliveirabr.stockwise.service.SeasonalityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,11 +15,15 @@ public class IndexController {
 
     private final ProductService productService;
     private final SaleService saleService;
+    private final SeasonalityService seasonalityService;
 
     @Autowired
-    public IndexController(ProductService productService, SaleService saleService) {
+    public IndexController(ProductService productService,
+                           SaleService saleService,
+                           SeasonalityService seasonalityService) {
         this.productService = productService;
         this.saleService = saleService;
+        this.seasonalityService = seasonalityService;
     }
 
     @GetMapping
@@ -28,6 +33,7 @@ public class IndexController {
 
         model.addAttribute("products", productService.findAll());
         model.addAttribute("sales", saleService.findAll());
+        model.addAttribute("seasonalities",seasonalityService.findAllSeasonality() );
         return "index";
     }
 
