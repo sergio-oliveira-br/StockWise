6265f08207b819d9a03734a40870e6ce1124e923
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-03-17 21:59:58 +0000

    Create SeasonalityController.java

---
+++ src/main/java/com/github/sergiooliveirabr/stockwise/controller/SeasonalityController.java
@@ -0,0 +1,32 @@
+package com.github.sergiooliveirabr.stockwise.controller;
+
+import com.github.sergiooliveirabr.stockwise.model.Seasonality;
+import com.github.sergiooliveirabr.stockwise.service.SeasonalityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/seasonality")
+public class SeasonalityController {
+
+    private final SeasonalityService seasonalityService;
+
+    @Autowired
+    public SeasonalityController(SeasonalityService seasonalityService) {
+        this.seasonalityService = seasonalityService;
+    }
+
+    @PostMapping("/save")
+    public String createSeasonality(@ModelAttribute Seasonality seasonality) {
+        seasonalityService.saveSeasonality(seasonality);
+        return "redirect:/";
+    }
+
+    @GetMapping("/all")
+    public String getAllSeasonalities(Model model) {
+        model.addAttribute("seasonalities", seasonalityService.findAllSeasonality());
+        return "index";
+    }
+}
