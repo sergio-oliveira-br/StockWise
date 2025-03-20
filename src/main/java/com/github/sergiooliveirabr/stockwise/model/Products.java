c24c0f5fa896a536d7e9207e6189996c3b5a72aa
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-03-20 09:17:06 +0000

    Update Products.java
    issue:
    # Lombok is not working properly
    
    act;
    -> Lombok removed. setters, getters toString added

--- src/main/java/com/github/sergiooliveirabr/stockwise/model/Products.java
+++ src/main/java/com/github/sergiooliveirabr/stockwise/model/Products.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Data
+//@Data
 @Table(name = "products")
 public class Products {
 
@@ -14,4 +14,27 @@ public class Products {
 
     private String description;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Products{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
